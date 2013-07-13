9d7b27ff2b5dafe35e5cba29915a1f78f23aa78b
Hao <hlin09pu@gmail.com>
2013-07-13 04:48:45 -0700

    update

---
+++ WebContent/EventCreateValidate.js
@@ -0,0 +1,87 @@
+var req;
+function validateForm(form) {
+	var name = document.forms["myForm"]["name"].value;
+	if (name == null || name == "") {
+		alert("Please enter a name.");
+		return false;
+	}
+	if (!isNaN(name)) {
+		alert("Please enter a valid name.");
+		return false;
+	}
+
+	var description = document.forms["myForm"]["description"].value;
+	var price = document.forms["myForm"]["price"].value;
+	var starttime = document.forms["myForm"]["starttime"].value;
+	var endtime = document.forms["myForm"]["endtime"].value;
+	var number = document.forms["myForm"]["number"].value;
+	var type = document.forms["myForm"]["type"].value;
+	var location = document.forms["myForm"]["location"].value;
+	var latitude = document.forms["myForm"]["latitude"].value;
+	var longitude = document.forms["myForm"]["longitude"].value;
+	var sendurl = "http://10.16.23.17:8080/ShareIt/EventCreate?name="
+		+ name + "&description=" + description + "&price="
+		+ price + "&starttime=" + starttime + "&endtime=" + endtime 
+		+ "&number=" + number + "&type=" + type + "&location=" + location
+		+ "&latitude=" + latitude + "&longitude=" + longitude;
+	alert(sendurl);
+	loadXMLDoc("http://10.16.23.17:8080/ShareIt/EventCreate?name="
+			+ name + "&description=" + description + "&price="
+			+ price + "&starttime=" + starttime + "&endtime=" + endtime 
+			+ "&number=" + number + "&type=" + type + "&location=" + location
+			+ "&latitude=" + latitude + "&longitude=" + longitude);
+
+}
+function loadXMLDoc(url) {
+	req = false;
+	if (window.XMLHttpRequest) {
+		try {
+			req = new XMLHttpRequest();
+		} catch (e) {
+			req = false;
+		}
+	} else if (window.ActiveXObject) {
+		try {
+			req = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			req = false;
+		}
+	}
+
+	if (req) {
+		req.onreadystatechange = processJSON;
+		req.open("GET", url, true);
+		req.send("");
+	}
+}
+function processJSON() {
+	if (req.readyState == 4) {
+		if (req.status == 200) {
+			var doc = JSON.parse(req.responseText);
+			var outputMsg = "";
+			if (doc.hotels.hotel.length == 0) {
+				document.getElementById("updateArea").innerHTML = "No Results Found";
+			} else {
+				// outputMsg += "<div id=fb-root></div>";
+				outputMsg += "<table class=output>";
+				outputMsg += "<th>Image</th><th>Name</th><th>Location</th><th>Rating out of 5</th><th>Reviews</th><th>Post to Facebook</th>";
+				outputMsg += "</table>";
+				document.getElementById("updateArea").innerHTML = outputMsg;
+			}
+		} else {
+			document.getElementById("updateArea").innerHTML = "Failed";
+		}
+	}
+
+}
+function processReqChange() {
+	if (req.readyState == 4) {
+		if (req.status == 200) {
+			var outMsg = req.responseXML;
+		} else {
+			var outMsg = "Failed";
+		}
+		document.getElementById("updateArea").innerHTML = outMsg;
+	}
+
+}
